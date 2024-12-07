6d12620ac6b866adbb6808e57c9f7b0c3ca431ff
Wroug <goldcc74@gmail.com>
2024-12-07 18:29:18 +0200

    Update install.ps1

--- install/install.ps1
+++ install/install.ps1
@@ -1,5 +1,8 @@
 
 $exists = Test-Path -Path $PROFILE
-
+if ($exists -eq $false) {
+	echo "We haven't found a profile at $profile"
+	$userchoice = Read-Host "[C] Create profile; [O] Another path for profile; [X] Exit; "
+}
 
 Get-Content .txt, inputFile2.txt | Set-Content joinedFile.txt
